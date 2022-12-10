51237449edcd4c45efe6c95ed516675a06888096
turner-kl <angra5296@gmail.com>
2022-12-10 15:23:16 +0900

    parser

--- src/compiler.js
+++ src/compiler.js
@@ -1,8 +1,32 @@
 const tokenizer = require("./tokenizer");
+const parser = require("./parser");
 
 module.exports = function compiler(input) {
   // 1. Lexical Analysis
   const tokens = tokenizer(input);
 
-  return tokens;
+  // 2. Syntactic Analysis
+  const ast = parser(tokens);
+
+  return JSON.stringify(ast);
+};
+const a = {
+  type: "Program",
+  body: [
+    {
+      type: "CallExpression",
+      name: "add",
+      params: [
+        { type: "NumberLiteral", value: "2" },
+        {
+          type: "CallExpression",
+          name: "sub",
+          params: [
+            { type: "NumberLiteral", value: "4" },
+            { type: "NumberLiteral", value: "3" },
+          ],
+        },
+      ],
+    },
+  ],
 };
---
+++ src/parser.js
@@ -0,0 +1,64 @@
+module.exports = function parser(tokens) {
+  let current = 0;
+
+  function walk() {
+    let token = tokens[current];
+
+    if (token.type === "number") {
+      current++;
+      return {
+        type: "NumberLiteral",
+        value: token.value,
+      };
+    }
+
+    if (token.type === "string") {
+      current++;
+
+      return {
+        type: "StringLiteral",
+        value: token.value,
+      };
+    }
+
+    if (token.type === "paren" && token.value === "(") {
+      token = tokens[++current];
+
+      let node = {
+        type: "CallExpression",
+        name: token.value,
+        params: [],
+      };
+
+      token = tokens[++current];
+      while (
+        token.type !== "paren" ||
+        (token.type === "paren" && token.value !== ")")
+      ) {
+        // we'll call the `walk` function which will return a `node` and we'll
+        // push it into our `node.params`.
+        node.params.push(walk());
+        token = tokens[current];
+      }
+
+      // Finally we will increment `current` one last time to skip the closing
+      // parenthesis.
+      current++;
+
+      // And return the node.
+      return node;
+    }
+    throw new TypeError(token.type);
+  }
+
+  let ast = {
+    type: "Program",
+    body: [],
+  };
+
+  while (current < tokens.length) {
+    ast.body.push(walk());
+  }
+
+  return ast;
+};
