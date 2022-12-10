fe34ec87a60b5ec5a70be1e857730a67a712542a
turner-kl <angra5296@gmail.com>
2022-12-10 16:20:55 +0900

    transformer

--- src/compiler.js
+++ src/compiler.js
@@ -1,6 +1,5 @@
 const tokenizer = require("./tokenizer");
 const parser = require("./parser");
-const traverser = require("./traverser");
 const transformer = require("./transformer");
 
 module.exports = function compiler(input) {
